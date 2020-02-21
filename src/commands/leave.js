1418e0a2e3735fb5c40f3f0ecdc8535524295940
Sergei Tomin <driver.by@gmail.com>
2020-02-21 23:26:41 +0300

    feat: clear all notification from server before leaving it

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,4 +1,7 @@
 const process = function(command, msg, dataStorage) {
+    const serverId = msg.guild.id;
+    const channelId = msg.channel.id;
+    dataStorage.subscriptionRemoveList(serverId, channelId);
     msg.channel.send(`Очень жаль расставаться, я буду скучать. Покидаю сервер`);
     msg.guild.leave();
 };
