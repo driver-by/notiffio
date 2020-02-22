435c742d2b8b91b00aff32c44b33758d930c596d
Siarhei Tomin <siarhei@hlpr.cc>
2020-02-22 10:21:54 +0300

    feat: limit max 50 channels per request to update goodgame info

--- src/services/goodgame.js
+++ src/services/goodgame.js
@@ -2,14 +2,24 @@ const AbstractService = require('./abstract-service');
 const ChannelDetails = require('../models/channel-details');
 const request = require('request-promise-native');
 
+const MAX_CHANNELS_PER_REQUEST = 50;
+
 class GoodgameService extends AbstractService {
     static getChannelStatuses(channels) {
-        return request({
-            uri: `https://goodgame.ru/api/getggchannelstatus?id=${channels.join(',')}&fmt=json`,
-            json: true,
-        }).then(response => {
-            let result = [];
+        let promises = [];
 
+        for (let i = 0; i < channels.length; i += MAX_CHANNELS_PER_REQUEST) {
+            const channelsPart = channels.slice(i, i + MAX_CHANNELS_PER_REQUEST);
+            promises.push(request({
+                uri: `https://goodgame.ru/api/getggchannelstatus?id=${channelsPart.join(',')}&fmt=json`,
+                json: true,
+            }));
+        }
+        return Promise.all(promises).then(response => {
+            let result = [];
+            // Flatten array and combine objects inside
+            response = Array.prototype.concat.apply([], response);
+            response = Object.assign.apply({}, response);
             Object.keys(response)
                 .forEach(i => {
                     const channel = response[i];
