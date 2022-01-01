bd25dc2d46ac981cb1e7b4f4ff572e3eb68f7cbd
Sergei Tomin <driver.by@gmail.com>
2022-01-01 19:36:03 +0300

    refactor: remove js files (were refactored to .ts)

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const Bot = require('./src/bot');
-const bot = new Bot();
-
-process.on("unhandledRejection", console.error);
--- src/commands/ping.js
+++
@@ -1,9 +0,0 @@
-const process = function(command, msg, dataStorage) {
-    const text = `Pong!`;
-
-    msg.reply(text);
-
-    return text;
-};
-
-module.exports = process;
