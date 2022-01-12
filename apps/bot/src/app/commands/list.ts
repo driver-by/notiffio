f45d5674c3ccfe9089b67f9bbdcfd0b7a129f69c
Sergei Tomin <driver.by@gmail.com>
2022-01-12 21:11:18 +0300

    fix: get subscriptions list

--- apps/bot/src/app/commands/list.ts
+++ apps/bot/src/app/commands/list.ts
@@ -9,21 +9,28 @@ export default async function list(command, msg, dataAccess: DataAccess) {
   const channelMap = {};
   let text = '';
   if (subscriptions?.length) {
+    let promises = [];
     subscriptions.forEach((subscription) => {
-      subscription.servers.forEach(async (server) => {
+      const promisesToAdd = subscription.servers.map(async (server) => {
+        let promise;
         if (serverId === server.serverId) {
           let channelName;
           if (channelMap[server.channelId]) {
             channelName = channelMap[server.channelId];
           } else {
-            channelName = await msg.client.channels.fetch(server.channelId);
+            promise = msg.client.channels.fetch(server.channelId);
+            const channel = await promise;
+            channelName = channel.name;
             channelMap[server.channelId] = channelName;
           }
           map[channelName] = map[channelName] || [];
           map[channelName].push(subscription.name);
+          return promise || Promise.resolve();
         }
       });
+      promises = promises.concat(promisesToAdd);
     });
+    await Promise.all(promises);
     Object.keys(map).forEach((channelName) => {
       text += `#${channelName}\n    ` + map[channelName].join(',\n    ') + '\n';
     });
